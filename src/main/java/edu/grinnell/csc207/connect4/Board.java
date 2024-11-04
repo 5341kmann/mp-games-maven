908290e0a5ae546428da96cfedf70963a84601c6
sarah_deschamps <deschamps.sias@gmail.com>
2024-11-04 11:51:30 -0600

    Filled in the Board class and added documentation.

--- src/main/java/edu/grinnell/csc207/connect4/Board.java
+++ src/main/java/edu/grinnell/csc207/connect4/Board.java
@@ -2,22 +2,76 @@ package edu.grinnell.csc207.connect4;
 
 import edu.grinnell.csc207.util.MatrixV0;
 
+/**
+ * A class that implements a Connect 4 Board.
+ *
+ * @author Sarah Deschamps
+ * @author Grant Sackmann
+ */
 public class Board {
+  /**
+   * The game board matrix.
+   */
   private MatrixV0<Character> gameBoard;
 
+  /**
+   * The constant width of the board.
+   */
+  private static final int BOARD_WIDTH = 7;
+
+  /**
+   * The constant height of the board.
+   */
+  private static final int BOARD_HEIGHT = 6;
+
+  /**
+   * The board constructor. Constructs a board of characters with defualt
+   * height and width, filling ' ' as the default character.
+   */
   public Board() {
-    this.gameBoard = new MatrixV0<Character>(7, 6);
+    this.gameBoard = new MatrixV0<Character>(BOARD_WIDTH, BOARD_HEIGHT, ' ');
   } // Board()
 
+  /**
+   * Returns the piece at the specified row and column.
+   * @param row
+   *  The row the piece is in.
+   * @param col
+   *  The column the piece is in.
+   * @return the piece in the given space.
+   */
   public Character getPiece(int row, int col) {
     return gameBoard.get(row, col);
   } // getPiece(int, int)
 
+  /**
+   * Fills the space at the given row and column with the given value.
+   * @param row
+   *  The given row.
+   * @param col
+   *  The given height.
+   * @param piece
+   *  The character to fill the space with.
+   */
   public void setPiece(int row, int col, Character piece) {
     gameBoard.set(row, col, piece);
   } // setPiece(int, int, Character)
 
-  public void drawBoard() {
-    // STUB
+  /**
+   * Builds a string representing the board.
+   * @return the string representation of the board.
+   */
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("+-+-+-+-+-+-+-+\n");
+    for (int r = 0; r < BOARD_HEIGHT; r++) {
+      sb.append("|");
+      for (int c = 0; c < BOARD_WIDTH; c++) {
+        sb.append(gameBoard.get(r, c) + "|");
+      } // for
+      sb.append("\n");
+    } // for
+    sb.append("+-+-+-+-+-+-+-+\n");
+    return sb.toString();
   } // drawBoard()
-}
+} // class Board
