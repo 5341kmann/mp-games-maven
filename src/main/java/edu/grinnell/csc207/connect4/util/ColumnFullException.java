4834a3ba785de99d237e286c1e0420f3bce66be3
[Grant Sackmann]] <[grant.sackmann@icloud.com]>
2024-11-05 18:11:45 -0600

    Updated GameLogic added javadoc comments

---
+++ src/main/java/edu/grinnell/csc207/connect4/util/ColumnFullException.java
@@ -0,0 +1,7 @@
+package edu.grinnell.csc207.connect4.util;
+
+public class ColumnFullException extends RuntimeException {
+  public ColumnFullException(String message) {
+    super(message);
+  }
+}
